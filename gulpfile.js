4d9940d71ab667fd70da3ac7851caf1031e9e983
StevenNguyen <fall.legendary1992@gmail.com>
2018-08-11 00:10:52 +0700

    complete

--- gulpfile.js
+++ gulpfile.js
@@ -9,93 +9,135 @@ var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 
 // Copy IMG
-gulp.task('copy', function () {
-  return gulp.src([
-      './src/img/**.*',
-    ])
-    .pipe(gulp.dest('./dist/img'));
+gulp.task('copy', function() {
+    return gulp.src([
+            './src/img/**.*',
+        ])
+        .pipe(gulp.dest('./dist/img'));
 });
 
+gulp.task('copy-js', function() {
+    return gulp.src([
+            'bower_components/jquery-circle-progress/dist/circle-progress.min.js',
+        ])
+        .pipe(gulp.dest('./dist/js'));
+});
+
+//fontawesome
+
+/*gulp.task('copy-fontawesomecss', function() {
+    return gulp.src([
+            'bower_components/font-awesome/web-fonts-with-css/css/**.*'
+        ])
+        .pipe(gulp.dest('./dist/css/font-awesome'));
+});
+
+gulp.task('copy-fontawesome', function() {
+    return gulp.src([
+            'bower_components/font-awesome/web-fonts-with-css/webfonts/**.*'
+        ])
+        .pipe(gulp.dest('./dist/css/font-awesome/webfonts'));
+});*/
+
+//fontawesome
+
+gulp.task('copy-fontawesome', function() {
+    return gulp.src([
+            'node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+        ])
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('copy-font', function() {
+    return gulp.src([
+            'node_modules/@fortawesome/fontawesome-free/webfonts/**.*'
+        ])
+        .pipe(gulp.dest('./dist/webfonts'));
+});
+
+
 // Nối file CSS
-gulp.task('noifile-css', function () {
-  return gulp.src([
-      'bower_components/bootstrap/dist/css/bootstrap.min.css',
-    ])
-    .pipe(concat('thuvien.css'))
-    .pipe(gulp.dest('./dist/css'));
+gulp.task('noifile-css', function() {
+    return gulp.src([
+            'bower_components/bootstrap/dist/css/bootstrap.min.css',
+        ])
+        .pipe(concat('thuvien.css'))
+        .pipe(gulp.dest('./dist/css'));
 });
 
 // Nối file JS
-gulp.task('noifile-js', function () {
-  return gulp.src([
-      'bower_components/jquery/dist/jquery.slim.min.js',
-      'bower_components/popper.js/dist/umd/popper.min.js',
-      'bower_components/bootstrap/dist/js/bootstrap.min.js',
-    ])
-    .pipe(concat('thuvien.js'))
-    .pipe(gulp.dest('./dist/js'));
+gulp.task('noifile-js', function() {
+    return gulp.src([
+            'bower_components/jquery/dist/jquery.slim.min.js',
+            'bower_components/popper.js/dist/umd/popper.min.js',
+            'bower_components/bootstrap/dist/js/bootstrap.min.js',
+            // 'bower_components/jquery-circle-progress/dist/circle-progress.min.js',
+
+        ])
+        .pipe(concat('thuvien.js'))
+        .pipe(gulp.dest('./dist/js'));
 });
 
 // Task này sẽ tìm tất cả file .sass trong thư mục src/styles sẽ build ra file .css ở thư mục dist/css
-gulp.task('taocss', function () {
-  return gulp.src('./src/styles/**/*.sass')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest('./dist/css'))
-    .pipe(browserSync.stream())
+gulp.task('taocss', function() {
+    return gulp.src('./src/styles/**/*.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write(''))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream())
 });
 
 // Task này sẽ tìm tất cả file .js trong thư mục src/scripts sẽ build ra file .js ở thư mục dist/js
-gulp.task('taojs', function () {
-  return gulp.src('./src/scripts/**/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(babel({
-      presets: ['env']
-    }))
-    .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest('./dist/js'));
+gulp.task('taojs', function() {
+    return gulp.src('./src/scripts/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(sourcemaps.write(''))
+        .pipe(gulp.dest('./dist/js'));
 });
 
 // Task này sẽ tìm tất cả file .pug trong thư mục src/templates sẽ build ra file .html ở thư mục dist
 gulp.task('taohtml', function buildHTML() {
-  return gulp.src([
-      './src/template/**/*.pug',
-      '!./src/template/{**/\_*,**/\_*/**}.pug'
-    ])
-    .pipe(pug({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./dist'));
+    return gulp.src([
+            './src/template/**/*.pug',
+            '!./src/template/{**/\_*,**/\_*/**}.pug'
+        ])
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./dist'));
 });
 
 // Task này có nhiệm vụ theo dõi mọi thay đổi trên hệ thống
-gulp.task('theodoi', function () {
-  gulp.watch('./src/styles/**/*.sass', ['taocss']);
-  gulp.watch('./src/template/**/*.pug', ['taohtml']);
-  gulp.watch('./src/scripts/**/*.js', ['taojs']);
-  gulp.watch("./dist/**/*.*").on('change', browserSync.reload);
+gulp.task('theodoi', function() {
+    gulp.watch('./src/styles/**/*.sass', ['taocss']);
+    gulp.watch('./src/template/**/*.pug', ['taohtml']);
+    gulp.watch('./src/scripts/**/*.js', ['taojs']);
+    gulp.watch("./dist/**/*.*").on('change', browserSync.reload);
 });
 
 // Static server
-gulp.task('browser-sync', function () {
-  browserSync.init({
-    server: {
-      baseDir: "./dist"
-    }
-  });
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
 });
 
 // Lệnh mặc định của Gulp
-gulp.task('default', function () {
-  gulp.start([
-    'copy',
-    'noifile-css',
-    'noifile-js',
-    'taocss',
-    'taojs',
-    'taohtml',
-    'theodoi',
-    'browser-sync'
-  ]);
+gulp.task('default', function() {
+    gulp.start([
+        'copy',
+        'noifile-css',
+        'noifile-js',
+        'taocss',
+        'taojs',
+        'taohtml',
+        'theodoi',
+        'browser-sync'
+    ]);
 });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 console.log('FullStack Vietnam')
+
+
+$('#circle1').circleProgress({
+  value: .9,
+  size: 140,
+  startAngle: -Math.PI / 4 * 2,
+  fill: {
+      gradient: ["#30bae7"]
+  }
+});
+
+$('#circle2').circleProgress({
+  value: 0.75,
+  size: 140,
+  startAngle: -Math.PI / 4 * 2,
+  fill: {
+      gradient: ["#d74680"]
+  }
+});
+
+$('#circle3').circleProgress({
+  value: 0.70,
+  size: 140,
+  startAngle: -Math.PI / 4 * 2,
+  fill: {
+      gradient: ["#17c2a4"]
+  }
+});
+
+$('#circle4').circleProgress({
+  value: 0.85,
+  size: 140,
+  startAngle: -Math.PI / 4 * 2,
+  fill: {
+      gradient: ["#eb7d4b"]
+  }
+});
